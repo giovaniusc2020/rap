8cf9bc0bd2bb9277e0ea977e2f4ffe87c5f75ed7
jonathanimbaquin <steven007771@hotmail.com>
2021-04-14 13:48:16 -0500

    todos los json del abecedario completado, get de palabras aleatorias funcionando

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,22 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+
+    
+        $data = file_get_contents("hola.json");
+        $palabras = json_decode($data, true);
+ 
+        foreach ($palabras as $palabra) {
+      echo '<pre>';
+    print_r($palabra);
+    echo '</pre>';
+
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/palabras/{letra}', 'PalabraController@palabrab_aleatoria');
